620257bf32f2a17b1e78957c6369b5efb3549aca
北村真依子 <maicotch@gmail.com>
2019-01-27 10:52:25 +0900

    Initial Commit

---
+++ Accelerometor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Accelerometor
+//
+//  Created by 北村真依子 on 2019/01/27.
+//  Copyright © 2019 myname. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
